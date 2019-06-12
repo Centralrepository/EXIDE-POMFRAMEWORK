44db485fc1f809a7113dd25eec3da5f66bc5ef93
VanithaC <vanitha@prakat.com>
2019-06-12 16:21:50 +0530

    initial commit till 12june

---
+++ Exide_POM/src/main/java/com/prakat/Generic/Helper/ConstantHelper.java
@@ -0,0 +1,11 @@
+package com.prakat.Generic.Helper;
+
+public interface ConstantHelper {
+		String FilePath = "Resources\\data\\data.xlsx";
+		String ExpPageTitle="Sprint 2.0";
+		String BrowserType ="chrome";
+		String AppURL="https://host19.exidelife.in/sprint2";
+
+	}
+  
+
